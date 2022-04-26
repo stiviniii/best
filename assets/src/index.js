fb082d7f661f8c085fed128cdb874f1045df6811
stiviniii <info@steveayo.com>
2022-04-26 17:15:11 +0300

    after initial setup and configurations

---
+++ assets/src/index.js
@@ -0,0 +1,3 @@
+import "../../sass/style.scss";
+import "../js/navigation.js";
+import "../js/customizer.js";
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = {
+    entry: './assets/src/index.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'assets/dist')
+    },
+    plugins: [],
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    }
+}
+
+if (currentTask == "build") {
+    config.mode = "production"
+    config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
+    config.plugins.push(new MiniCssExtractPlugin({ filename: "main.css"}));
+}
+module.exports = config
