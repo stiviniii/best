9d31aaae3348f9327fa4bc09d1e65de5f9bca119
stiviniii <info@steveayo.com>
2022-04-27 17:36:22 +0300

    Updates on links and more

--- functions.php
+++ functions.php
@@ -138,10 +138,11 @@ add_action( 'widgets_init', 'best_widgets_init' );
  * Enqueue scripts and styles.
  */
 function best_scripts() {
-	wp_enqueue_style( 'best-style', get_stylesheet_uri(), array(), _S_VERSION );
+	// wp_enqueue_style( 'best-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'best-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'best-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	// wp_enqueue_script( 'best-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'best-navigation', get_template_directory_uri() . '/assets/dist/bundled.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
